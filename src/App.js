beec63252417fde12c0d8013e86b190663286177
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-08 01:23:26 -0400

    Memory game, baby

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import MemoryCard from './Components/MemoryCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function generateDeck() {
+  const symbols = [`∆`, `ß`, `£`, `§`, `•`, `$`, `+`, `ø`];
+  const deck = [];
+  for (let i = 0; i < 16; i++) {
+    const card = {isFlipped: false, symbol: symbols[i % 8]};
+    deck.push(card);
+  }
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return a;
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: [],
+    }
+  };
+  
+  pickCard = (cardIndex) => {
+    if (this.state.deck[cardIndex].isFlipped){
+      return;
+    }
+    const cardToFlip = {...this.state.deck[cardIndex]};
+    
+    cardToFlip.isFlipped = true;
+
+  const newDeck = this.state.deck.map((card, index) => {
+    if (cardIndex === index) {
+      return cardToFlip;
+    }
+    return card;
+  });
+
+  let newPickedCards = this.state.pickedCards.concat(cardIndex);
+
+  if (newPickedCards.length === 2) {
+    const card1Index = newPickedCards[0];
+    const card2Index = newPickedCards[1];
+    if (newDeck[card1Index].symbol !== newDeck[card2Index].symbol) {
+      setTimeout(() => {
+        this.unflipCards(card1Index, card2Index)
+      },1000);
+    }
+    newPickedCards = [];
+  }
+
+  this.setState({
+    deck: newDeck, 
+    pickedCards: newPickedCards
+  });
+};
+
+  unflipCards = (card1Index, card2Index) => {
+    const card1 = {...this.state.deck[card1Index]};
+    const card2 = {...this.state.deck[card2Index]};
+    card1.isFlipped = false;
+    card2.isFlipped = false;
+
+    const newDeck = this.state.deck.map((card, index) => {
+      if (card1Index === index) {
+        return card1;
+      }  
+      if (card2Index === index) {
+        return card2;
+      } 
+      return card
+    });
+
+    this.setState({
+      deck: newDeck
+    });
+  }
+
+
+
+  render() {
+    const cardsJSX = this.state.deck.map((card, index) => {
+      return (
+        <MemoryCard 
+          key={index}
+          symbol={card.symbol}
+          isFlipped={card.isFlipped}
+          pickCard={this.pickCard.bind(this, index)}
+        />)
+      });
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              Memory Game
+            </h1>
+            <h3>
+              Match Cards To Win
+            </h3>
+          </header>
+          <div>
+            {cardsJSX.slice(0,4)}
+          </div>
+          <div>
+            {cardsJSX.slice(4,8)}
+          </div>
+          <div>
+            {cardsJSX.slice(8,12)}
+          </div>
+          <div>
+            {cardsJSX.slice(12,16)}
+          </div>
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/Components/MemoryCard.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import './MemoryCard.css';
+
+class MemoryCard extends Component {
+    
+    render() {
+        const memoryCardInnerClass = this.props.isFlipped ? 'MemoryCardInner flipped' : 'MemoryCardInner';  
+        return (
+            <div className="MemoryCard" onClick={this.props.pickCard}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="card"/>
+                    </div>
+                    <div className="MemoryCardFront">
+                        {this.props.symbol}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MemoryCard;
